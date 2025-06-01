58f68bc1f9ba730c772bad30209aaa6b0db412f3
TuMinhHung0778 <tuminhhung0901@gmail.com>
2025-06-01 11:21:15 +0700

    update setup

--- KienThucTrinhDuyetVaAPI/app.js
+++
@@ -1,6 +0,0 @@
-const xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    console.log(this.readyState);
-}
-xhttp.open('GET', 'https://reqres.in/api/users?page=2', true)
-xhttp.send()
